66dbd5398bfe0bc49cb55634cd77048858c23a39
Adalto Junior <herdeiro.junior@gmail.com>
2019-12-07 15:35:21 -0300

    Changing file location

---
+++ src/app/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import BitCoinPriceCard from './components/BitCoinPriceCard'
+import { getLastPrice } from './api/bitcoinpricesapi';
+
+class App extends React.Component<{}, {lastPrice: number}> {
+  state = {
+    lastPrice: 0
+  }
+  componentDidMount() {
+    getLastPrice().then((data) => {
+      this.setState({
+        lastPrice: data.market_price_usd || 0
+      })
+    })
+  }
+  render = () => (
+    <div className="App">
+      <BitCoinPriceCard value={this.state.lastPrice} coinName="Bitcoin" />
+    </div>
+  );
+}
+
+export default App;
