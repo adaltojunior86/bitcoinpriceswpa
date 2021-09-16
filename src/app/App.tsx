b53bc085a4030871eaf8229eb781b97e7810f238
Adalto Junior <herdeiro.junior@gmail.com>
2021-09-15 21:45:56 -0300

    Updating dependencies.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -16,7 +16,7 @@ class App extends React.Component<{}, {lastPrice: number}> {
   }
   render = () => (
     <div className="App">
-      <BitCoinPriceCard value={this.state.lastPrice} coinName="Bitcoin" />
+      <BitCoinPriceCard value={this.state.lastPrice} />
     </div>
   );
 }
